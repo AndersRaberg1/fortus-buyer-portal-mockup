a31b4d47c276c37e57f0458bf44b81becda7411d
AndersRaberg1 <99084665+AndersRaberg1@users.noreply.github.com>
2026-02-12 12:18:20 +0100

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-// Singleton – klient skapas bara en gång i browsern
+// Singleton – skapas bara en gång i browsern
 let supabaseClient: any = null;
 
 if (typeof window !== 'undefined' && !supabaseClient) {
